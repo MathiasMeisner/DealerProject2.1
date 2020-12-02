83d311b1631dab22d1fcb873db389207805d20d5
jakobln <62882745+jakobln@users.noreply.github.com>
2020-12-02 13:49:07 +0100

    Opdateret BilmærkeViewModel.cs

--- CarDealerProject/BilmærkeViewModel.cs
+++ CarDealerProject/BilmærkeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BilmærkeViewModel;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -18,7 +19,7 @@ namespace CarDealerProject
 
         //Ensure tha this is the same URL as applied under 
         //Properties->Web->Project URL in the web service project 
-        //const string serverUrl = "http://localhost:49970/";
+        const string serverUrl = "http://localhost:53751/";
 
         public ObservableCollection<Bil> OC_bilmærker { get; set; }
 
@@ -26,23 +27,12 @@ namespace CarDealerProject
         public int AntalBlomst { get => antalBlomst; set => antalBlomst = value; }
         public string FarveBlomst { get => farveBlomst; set => farveBlomst = value; }*/
 
-        private string jsonBilmærker;
-        public string JsonBilmærker
-        {
-            get { return jsonBilmærker; }
-            set { jsonBilmærker = value; }
-        }
+        //public OrdreBlomst SelectedOrdreBlomst { get; set; }
 
-        StorageFolder localfolder = null;
-
-        private readonly string filnavn = "bilmærker1.json";
-
-        public OrdreBlomst SelectedOrdreBlomst { get; set; }
-
-        /*public RelayCommand AddNyBlomst { get; set; }
-        public RelayCommand SletSelectedBlomst { get; set; }
+        //public RelayCommand AddNyBlomst { get; set; }
+        //public RelayCommand SletSelectedBlomst { get; set; }
         public RelayCommand GemData { get; set; }
-        public RelayCommand HentData { get; set; }*/
+        public RelayCommand HentData { get; set; }
 
         public BilmærkeViewModel()
         {
@@ -58,12 +48,9 @@ namespace CarDealerProject
 
             SelectedOrdreBlomst = new OrdreBlomst();*/
 
-            localfolder = ApplicationData.Current.LocalFolder;
-
-            GemData = new RelayCommand(GemDataTilDiskAsync);
-
+            //GemData = new RelayCommand(GemDataTilDiskAsync);
             HentData = new RelayCommand(HentDataFraDiskAsync);
-            DanData();
+            //DanData();
         }
 
 
@@ -121,54 +108,29 @@ namespace CarDealerProject
             return OC_blomster.Count > 0;
         }*/
 
-        ///// <summary>
-        ///// metode som modtager en string af json og deserialiserer til objekter af OrdreBlomst
-        ///// </summary>
-        ///// <param name="jsonText"></param>
-        //private void IndsætJson(string jsonText)
-        //{
-        //    List<OrdreBlomst> nyListe =  JsonConvert.DeserializeObject<List<OrdreBlomst>>(jsonText);
-
-        //    foreach (var blomst in nyListe)
-        //    {
-        //        this.OC_blomster.Add(blomst);
-        //    }
-        //    SletSelectedBlomst.RaiseCanExecuteChanged();
-        //}
-
-        /// <summary>
-        /// Giver mig Jsonformat for OC_blomster objektet
-        /// </summary>
-        /// <returns></returns>
-        private string GetJson()
-        {
-            string json = JsonConvert.SerializeObject(OC_bilmærker);
-            return json;
-        }
-
         /// <summary>
         /// Gemmer json data fra liste i localfolder
         /// </summary>
-        private async void GemDataTilDiskAsync()
+        /*private async void GemDataTilDiskAsync()
         {
-            this.jsonBilmærker = GetJson();
+            //this.jsonBilmærker = GetJson();
 
-            StorageFile file = await localfolder.CreateFileAsync(filnavn, CreationCollisionOption.ReplaceExisting);
+            //StorageFile file = await localfolder.CreateFileAsync(filnavn, CreationCollisionOption.ReplaceExisting);
 
-            await FileIO.WriteTextAsync(file, this.jsonBilmærker);
+            //await FileIO.WriteTextAsync(file, this.jsonBilmærker);
 
             // await FileIO.WriteTextAsync(file, GetJson());
-            SletSelectedBlomst.RaiseCanExecuteChanged();
-        }
+            /*SletSelectedBlomst.RaiseCanExecuteChanged();
+        }*/
 
         /// <summary>
         /// Henter en json fil fra disken 
         /// </summary>
-        private async void HentDataFraDiskAsync()
+        private void HentDataFraDiskAsync()
         {
             OC_bilmærker.Clear();
             List<Bil> nyListe = new List<Bil>();
-            nyListe = await PersistencyService.HentDataFraDiskAsyncPS();
+            //nyListe = await PersistencyService.HentDataFraDiskAsyncPS();
 
             //Setup client handler
             HttpClientHandler handler = new HttpClientHandler();
@@ -186,25 +148,25 @@ namespace CarDealerProject
                 try
                 {
                     //Get all the flower orders from the database
-                    var flowerOrderResponse = client.GetAsync("api/flowerorders").Result;
+                    var flowerOrderResponse = client.GetAsync("api/bils").Result;
 
                     //Check response -> throw exception if NOT successful
                     flowerOrderResponse.EnsureSuccessStatusCode();
 
                     //Get the hotels as a ICollection
-                    var orders = flowerOrderResponse.Content.ReadAsAsync<ICollection<FlowerOrder>>().Result;
+                    var orders = flowerOrderResponse.Content.ReadAsAsync<ICollection<Bildb>>().Result;
 
                     foreach (var order in orders)
                     {
 
-                        this.OC_blomster.Add(new OrdreBlomst(order.FlowerName, order.Quantity, order.Color));
+                        this.OC_bilmærker.Add(new Bil(order.BilID, order.ForhandlerID, order.BilMærke, order.BilModel, order.BilUdstyr, order.BilMotor));
                     }
 
                     /*foreach (var blomst in nyListe)
                     {
                         this.OC_blomster.Add(blomst);
                     }*/
-                    SletSelectedBlomst.RaiseCanExecuteChanged();
+                    //SletSelectedBlomst.RaiseCanExecuteChanged();
                 }
                 catch
                 {
