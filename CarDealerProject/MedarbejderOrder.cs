0a095c83e295f1595ac431969b8b3fdd53beb308
jakobln <62882745+jakobln@users.noreply.github.com>
2020-12-02 11:58:07 +0100

    Opdateret MedarbejderOrder.cs

--- CarDealerProject/MedarbejderOrder.cs
+++ CarDealerProject/MedarbejderOrder.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CarDealerProject
 {
-    public class Medarbejder
+    /*public class Medarbejder : INotifyPropertyChanged
     {
-    }
+        private int medarbejderID;
+        public int MedarbejderID
+        {
+            get { return medarbejderID; }
+            set
+            {
+                medarbejderID = value;
+                OnPropertyChanged();
+            }
+        }
+    }*/
 }
