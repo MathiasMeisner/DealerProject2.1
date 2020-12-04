fdfb3e5fdd7718dbeba864ed85032a9cbe3a416c
jakobln <62882745+jakobln@users.noreply.github.com>
2020-12-04 13:31:45 +0100

    Opdateret MedarbejderOrder og tilføjet KundeOrder

---
+++ CarDealerProject/KundeOrder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerProject
+{
+    public class Kunde : INotifyPropertyChanged
+    {
+        private string kundeEmail;
+        public string KundeEmail
+        {
+            get { return kundeEmail; }
+            set
+            {
+                kundeEmail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string navn;
+        public string Navn
+        {
+            get { return navn; }
+            set
+            {
+                navn = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int telefon;
+        public int Telefon
+        {
+            get { return telefon; }
+            set
+            {
+                telefon = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Kunde(string kundeEmail, string navn, int telefon)
+        {
+            this.KundeEmail = kundeEmail;
+            this.Navn = navn;
+            this.Telefon = telefon;
+        }
+    }
+}
--- CarDealerProject/MedarbejderOrder.cs
+++ CarDealerProject/MedarbejderOrder.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CarDealerProject
 {
-    /*public class Medarbejder : INotifyPropertyChanged
+    public class Medarbejder : INotifyPropertyChanged
     {
         private int medarbejderID;
         public int MedarbejderID
@@ -20,5 +20,65 @@ namespace CarDealerProject
                 OnPropertyChanged();
             }
         }
-    }*/
+
+        private int forhandlerID;
+        public int ForhandlerID
+        {
+            get { return forhandlerID; }
+            set
+            {
+                forhandlerID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string medarbejderNavn;
+        public string MedarbejderNavn
+        {
+            get { return medarbejderNavn; }
+            set
+            {
+                medarbejderNavn = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int medarbejderTelefon;
+        public int MedarbejderTelefon
+        {
+            get { return medarbejderTelefon; }
+            set
+            {
+                medarbejderTelefon = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int superBruger;
+        public int SuperBruger
+        {
+            get { return superBruger; }
+            set
+            {
+                superBruger = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Medarbejder(int medarbejderID, int forhandlerID, string medarbejderNavn, int medarbejderTelefon, int superBruger)
+        {
+            this.MedarbejderID = medarbejderID;
+            this.ForhandlerID = forhandlerID;
+            this.MedarbejderNavn = medarbejderNavn;
+            this.MedarbejderTelefon = medarbejderTelefon;
+            this.SuperBruger = superBruger;
+        }
+    }
 }
