d38acdf61c791d522fd129f919b99dc8cc7869dd
jakobln <62882745+jakobln@users.noreply.github.com>
2020-12-11 12:59:29 +0100

    Tilføjet Page2 og HyperlinkButton

--- CarDealerProject/Page2.xaml.cs
+++ CarDealerProject/Page2.xaml.cs
@@ -13,25 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace CarDealerProject
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Page2 : Page
     {
-        public BilmaerkeViewModel ViewModel { get; set; }
-        public MainPage()
+        public Page2()
         {
             this.InitializeComponent();
-            ViewModel = new BilmaerkeViewModel();
         }
 
-        private void Hyperlinkbutton_Click(object sender, RoutedEventArgs e)
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(Page2));
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
